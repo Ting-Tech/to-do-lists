fd2cd8aa2b5d343bc1f048d2d68a7d630ccad3a2
AllenTing <67228710+Ting-Tech@users.noreply.github.com>
2023-11-25 16:31:47 +0800

    Finish tasks board

--- my-react-app/src/components/Header/index.jsx
+++ my-react-app/src/components/Header/index.jsx
@@ -1,7 +1,20 @@
+import styles from './header.module.css';
+import ToDoLogo from '../../assets/ToDoLogo.svg';
+import { LiaPlusCircleSolid } from "react-icons/lia";
+
+
 export function Header() {
     return (
-        <header>
-            <p>header</p>
+        <header className={styles.header}>
+            <img src={ToDoLogo} />
+
+            <form className={styles.newTaskForm}>
+                <input placeholder="add a new task" type="text" />
+                <button>
+                    Create
+                    <LiaPlusCircleSolid size={20} />
+                </button>
+            </form>
         </header>
     )
 }
